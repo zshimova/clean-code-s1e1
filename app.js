416cd76764f44254a533d782e084170bc9d0055d
Zlata <zshimova@bk.ru>
2025-10-20 23:29:33 +0300

    refactor: update HTML, CSS and JS according to rules 1.2. Media alternatives, 3.1. Single style for selectors naming in html-and-css.md

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("todo-app__item--editing");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("todo-app__item--editing");
 };
 
 
